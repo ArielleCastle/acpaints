19665c62388a51fcb5f0bf6273403a06a8e33d25
eyryell <aceysicastillo@gmail.com>
2024-08-19 13:46:51 +0800

    up

--- acpaints.js
+++ acpaints.js
@@ -1,10 +1,13 @@
+/*start- open login*/
 function toggleBodyBar() {
     var bodybar = document.querySelector('.bodybar');
     bodybar.style.display = (bodybar.style.display === 'none' || bodybar.style.display === '') ? 'flex' : 'none';
     document.body.classList.toggle('dark-background');
-
 }
+/*end*/
 
+
+/*start- transparent nav when scrolled*/
 document.addEventListener("scroll", function() {
     var nav = document.querySelector("nav");
     if (window.scrollY > 50) { // Adjust scroll distance as needed
@@ -13,3 +16,5 @@ document.addEventListener("scroll", function() {
         nav.classList.remove("nav-transparent");
     }
 });
+/*end- transparent nav when scrolled*/
+
